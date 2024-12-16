ad33b0aa8f1336eaef516aa608d8e08816ac6898
Izzettin Karasayar <izzettin.karasayar@std.yildiz.edu.tr>
2024-12-16 14:11:49 +0300

    Initial commit

---
+++ src/main/java/com/cmdotenter/VetCare/VetCareApplication.java
@@ -0,0 +1,13 @@
+package com.cmdotenter.VetCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmdotenter/VetCare/VetCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmdotenter.VetCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
